40d08e9f0680137dbda2869ee377cefcfad8b18e
NNahkan <hoksuz2424@gmail.com>
2023-11-16 13:57:50 -0500

    Prisma is endless broo

--- problems/p8.test.ts
+++ problems/p8.test.ts
@@ -4,27 +4,27 @@ import { clearDb, seedFixtures } from "../seed-helpers";
 import { findTheGrumpiestCriticId, findTheNicestCriticId } from "./p8";
 
 describe("p7", () => {
-  beforeEach(async () => {
-    await clearDb();
-  });
+	beforeEach(async () => {
+		await clearDb();
+	});
 
-  it("findGrumpiestCriticId should be a function", () => {
-    expect(findTheGrumpiestCriticId).toBeInstanceOf(Function);
-  });
+	it("findGrumpiestCriticId should be a function", () => {
+		expect(findTheGrumpiestCriticId).toBeInstanceOf(Function);
+	});
 
-  it("should return Peter", async () => {
-    const { users } = await seedFixtures();
+	it("should return Peter", async () => {
+		const { users } = await seedFixtures();
+		console.log("from test", users.peter.id)
+		expect(await findTheGrumpiestCriticId()).toEqual(users.peter.id);
+	});
 
-    expect(await findTheGrumpiestCriticId()).toEqual(users.peter.id);
-  });
+	it("findTheNicestCriticId should be a function", async () => {
+		expect(findTheNicestCriticId).toBeInstanceOf(Function);
+	});
 
-  it("findTheNicestCriticId should be a function", async () => {
-    expect(findTheNicestCriticId).toBeInstanceOf(Function);
-  });
+	it("the nicest critic should be andrey", async () => {
+		const { users } = await seedFixtures();
 
-  it("the nicest critic should be andrey", async () => {
-    const { users } = await seedFixtures();
-
-    expect(await findTheNicestCriticId()).toEqual(users.andrey.id);
-  });
+		expect(await findTheNicestCriticId()).toEqual(users.andrey.id);
+	});
 });
--- problems/p8.ts
+++ problems/p8.ts
@@ -1,10 +1,34 @@
-import { maxBy, minBy } from "remeda";
+import { maxBy, minBy, pipe } from "remeda";
 import { prisma } from "./prisma";
 
-// Always tell truths, don't you ever lie, to solve this problem, just try a `groupBy`
+const groupPeopleByScore = async () => {
+	return await prisma.starRating.groupBy({
+		by: ['userId'],
+		_avg: {
+			score: true
+		},
+		orderBy: {
+			_avg: {
+				score: "asc"
+				// if I am going to use minBy or maxBy, is it pointless to use orderBy ?
+			}
+		},
 
+
+
+		// take: 1
+	})
+}
+
+// Always tell truths, don't you ever lie, to solve this problem, just try a `groupBy`
 // find the critic with the lowest average score
-export const findTheGrumpiestCriticId = async () => {};
+export const findTheGrumpiestCriticId = () => {
+	return groupPeopleByScore().then((result) => minBy(result, (item) => item._avg.score)?.userId);
+	// return groupPeopleByScore().then((result) => pipe(result, minBy((item) => item._avg.score))?.userId);
+}
+
 
 // find the critic with the highest average score
-export const findTheNicestCriticId = async () => {};
+export const findTheNicestCriticId = () => {
+	return groupPeopleByScore().then((result) => result[result.length - 1].userId)
+};
